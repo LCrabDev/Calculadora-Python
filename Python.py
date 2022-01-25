ff6e7ccea49a223a5c45d803e9aaef9a44855896
LitleCrabDev <84681470+LitleCrabDev@users.noreply.github.com>
2022-01-24 21:03:29 -0300

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,15 +1,18 @@
-"""Esse código é uma versão simplificada de uma calculadora em Python"""
+# Esse código é uma versão de uma calculadora em Python
 
-# Irei criar uma função com o nome "calculadora" que recebe como parâmetro (valor1, valor2)
+""" Irei criar uma função com o nome "calculadora" que recebe como parâmetro (valor1, valor2)"""
+
 print('__CALCULADORA PYTHON__')
 print(22 * '-')
 def calculadora(valor1, valor2):
+
     # O usuário irá escolher os numeros.
     valor1 = int(input('Digite um valor: '))
     valor2 = int(input('Digite outro valor: '))
     print('Os operadores matematicos sao: (Soma (+), Subtracao (-), Multiplicacao (X), Divisao (/)')
 
     # Após a escolha, o usuário irá escolher o operador matemático.
+    
     operador = input('Digite um operador matemático: ')
     soma = valor1 + valor2
     subtracao = valor1 - valor2
@@ -17,11 +20,14 @@ def calculadora(valor1, valor2):
     divisao = valor1 / valor2
 
     #Para tratar as escolhas do usuário, criei uma lista com os operadores matemáticos.
+
     operadores_matematicos = ['+', '-', 'x', 'X', '/']
 
     # Agora o código irá tratar as informações escolhidas pelo usuário.
+
     ''' O código irá interpretar (Soma como ' + '), (Subtração como ' - ')
     (Multiplicação como ' x ' ou ' X ') e (Divisão como ' / ').'''
+
     if operador == '+':
         print('O resultado da soma foi: {}'.format(soma))
     elif operador == '-':
@@ -35,5 +41,6 @@ def calculadora(valor1, valor2):
         if operador != operadores_matematicos:
             print('Por favor, digite somente operadores matematicos de Soma, Subtracao, Multiplicacao ou Divisao')
 
-# A estrutura da função foi finalizada, basta executar como "Run Python File".
+# A estrutura da função foi finalizada, basta executar o código como "Run Python File".
+
 calculadora('valor1', 'valor2')
